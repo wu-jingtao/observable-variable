2489dd8c380591b833cdb938d7cb4e6faac35304
wujingtao <601595686@qq.com>
2018-08-20 23:34:34 +0800

    更新了一下项目结构

--- bin/Helper/Watch.d.ts
+++
@@ -1,7 +0,0 @@
-import { ObservableVariable } from "../ObservableDataType/ObservableVariable";
-/**
- * 观察一个或多个变量的改变。对于数组、Map、Set，添加或删除或更新元素操作也会触发。
- * 返回一个off方法用于取消监听变化
- */
-export declare function watch<T>(items: ObservableVariable<T>[], callback: (variables: ReadonlyArray<ObservableVariable<T>>) => void): () => void;
-//# sourceMappingURL=Watch.d.ts.map
