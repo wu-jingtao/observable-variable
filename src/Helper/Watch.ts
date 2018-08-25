679320e0ed78d50a655d19d9c063706e375f2d02
wujingtao <601595686@qq.com>
2018-08-26 01:24:23 +0800

    优化了一下 watch 方法

--- src/Helper/Watch.ts
+++ src/Helper/Watch.ts
@@ -4,8 +4,8 @@ import { ObservableVariable } from "../ObservableDataType/ObservableVariable";
  * 观察一个或多个变量的改变。对于数组、Map、Set，添加或删除或更新元素操作也会触发。   
  * 返回一个off方法用于取消监听变化
  */
-export function watch<T>(items: ObservableVariable<T>[], callback: (variables: ReadonlyArray<ObservableVariable<T>>) => void) {
-    function listener() { callback(items); }
+export function watch(items: ObservableVariable<any>[], callback: () => void) {
+    function listener() { callback(); }
 
     items.forEach((item: any) => {
         item.on('set', listener);
